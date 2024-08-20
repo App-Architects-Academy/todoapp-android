a774a8619e290867bf145a7d3f46d9a4d6913169
Parth Takkar <parthdroidworld@gmail.com>
2024-08-20 22:23:55 +0530

    Add colors, typography and shapes for Design System

--- app/src/main/java/academy/apparchitects/todoapp/ui/designsystem/DesignSystem.kt
+++
@@ -1,2 +0,0 @@
-package academy.apparchitects.todoapp.ui.designsystem
-
--- app/src/main/java/academy/apparchitects/todoapp/ui/theme/Theme.kt
+++ app/src/main/java/academy/apparchitects/todoapp/ui/theme/Theme.kt
@@ -1,58 +1,51 @@
 package academy.apparchitects.todoapp.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
-)
+@Stable
+data class DarkTheme(val isDark: Boolean = false)
 
-private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
-
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+val LocalTheme = staticCompositionLocalOf { DarkTheme() }
+val LocalAppColors = staticCompositionLocalOf {
+  getColors(false)
+}
 
 @Composable
 fun NotesAppTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
-        content: @Composable () -> Unit
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
+  val theme = DarkTheme(darkTheme)
+  val colors = getColors(darkTheme)
+  CompositionLocalProvider(
+    LocalTheme provides theme,
+    LocalAppColors provides colors,
+    LocalAppTypography provides appTypography,
+    LocalAppShapes provides appShapes
+  ) {
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+      content = content
     )
+  }
+}
+
+
+object AppTheme {
+  val typography: AppTypography
+    @Composable
+    get() = LocalAppTypography.current
+  val colors: AppColors
+    @Composable
+    get() = LocalAppColors.current
+  val theme: DarkTheme
+    @Composable
+    get() = LocalTheme.current
+  val shapes: AppShapes
+    @Composable
+    get() = LocalAppShapes.current
 }
